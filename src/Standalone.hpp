d7d68d8f03c44a812c4f4b404c65ac98383f1036
Abhinav Neelam <abhinavneelam949@gmail.com>
2019-01-20 21:53:23 -0800

    Made Standalone.hpp readable

--- src/Standalone.hpp
+++ src/Standalone.hpp
@@ -6,9 +6,9 @@ float gaussianRandom() {
 		v1 = 2.0f * (float)(rand() % 10000) / (10000) - 1.0f;
 		v2 = 2.0f * (float)(rand() % 10000) / (10000) - 1.0f;
 		s = v1 * v1 + v2 * v2;
-	} while (s >= 1.0f || s == 0);
+	} while (s >= 1.0f || s == 0.0f);
 
-	s = (float)pow((-2.0f * log(s)) / s, 0.5);
+	s = (float)pow((-2.0f * log(s)) / s, 0.5f);
 
 	return v1 * s;
 }
