e6edc6f73a1aa4a8597373307c7466bc57d34e38
Abhinav Neelam <abhinavneelam949@gmail.com>
2019-01-23 10:46:46 -0800

    Changed ColVector and refactored code

--- include/ColVector.h
+++ include/ColVector.h
@@ -1,7 +1,7 @@
 #ifndef COL_VECTOR
 #define COL_VECTOR
 
-#include "Matrix.h"
+#include "Matrix.hpp"
 template<typename T>
 class ColVector : public Matrix<T>
 {
@@ -12,12 +12,10 @@ public:
 	inline void resize(const size_t size);
 
 	//INSERT
+	inline void insert(const ColVector<T>& object);
 	inline void insert(size_t s, const T val);
 	inline void insert_zeros(size_t s);
 	inline void insert_ones(size_t s);
-
-	//MOVE OPERATOR
-	inline ColVector<T>& operator= (ColVector<T>&& m);
 
 	//DEFAULT CONSTRUCTOR AND DESTRUCTOR
 	inline ColVector(size_t size);
@@ -25,9 +23,7 @@ public:
 	inline ColVector(const std::initializer_list<T>& list);
 	inline ColVector(const std::vector<T>& list);
 	inline ColVector(size_t size, const T* list);
-	inline ColVector(ColVector<T>&& c);
 	inline ColVector();
-	inline ~ColVector();
 };
 
 #endif // !COL_VECTOR
--- src/ColVector.hpp
+++ src/ColVector.hpp
@@ -6,11 +6,6 @@ ColVector<T>::ColVector() {
 }
 
 template<typename T>
-ColVector<T>::~ColVector() {
-
-}
-
-template<typename T>
 inline void ColVector<T>::set_size(const size_t size)
 {
 	Matrix<T>::set_size(size, 1);
@@ -29,12 +24,21 @@ inline void ColVector<T>::resize(const size_t size)
 }
 
 template<typename T>
+inline void ColVector<T>::insert(const ColVector<T>& object)
+{
+	auto& mat = Matrix<T>::matrix;
+	mat.insert(mat.end(), object.matrix.begin(), object.matrix.end());
+
+	this->reshape(object.size + Matrix<T>::size);
+}
+
+template<typename T>
 inline void ColVector<T>::insert(size_t s, const T val)
 {
-	this->resize(s);
-	for (size_t i = 0;i < s;++i) {
-		getMatrix(*this).emplace_back(val);
-	}
+	auto& mat = Matrix<T>::matrix;
+	mat.insert(mat.end(), s, val);
+
+	this->reshape(s + Matrix<T>::size);
 }
 
 template<typename T>
@@ -47,14 +51,6 @@ template<typename T>
 inline void ColVector<T>::insert_ones(size_t s)
 {
 	this->insert(s, 1);
-}
-
-template<typename T>
-inline ColVector<T>& ColVector<T>::operator=(ColVector<T>&& m)
-{
-	Matrix<T>::operator=(std::move(m)); //no clue
-
-	return *this;
 }
 
 template<typename T>
@@ -81,12 +77,6 @@ inline ColVector<T>::ColVector(const std::vector<T>& list) : Matrix<T>(list.size
 
 template<typename T>
 inline ColVector<T>::ColVector(size_t size, const T * list) : Matrix<T>(size, 1, list)
-{
-
-}
-
-template<typename T>
-inline ColVector<T>::ColVector(ColVector<T>&& c) : Matrix<T>(std::move(c))
 {
 
 }
