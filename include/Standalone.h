e47d128e2e08f7e1ac24e52e5f6b48e96dddc303
Abhinav Neelam <abhinavneelam949@gmail.com>
2019-01-23 15:33:53 -0800

    Started work on standalone functions using namespaces

--- include/Standalone.h
+++ include/Standalone.h
@@ -2,12 +2,23 @@
 #define STANDALONE_H
 
 #define _USE_MATH_DEFINES
+#define templ template<typename T>
+
+template<typename T>
+class Matrix;
 
 #include <cmath>
 #include <limits>
 
-float gaussianRandom();
-float uniformFloatRandom();
-int uniformIntRandom();
+namespace random {
+	float gaussianRandom();
+	float uniformFloatRandom();
+	int uniformIntRandom(int max = INT_MAX);
+}
+
+namespace tenseopr {
+	templ Matrix<T> abs(const Matrix<T>& m);
+	templ T accu(const Matrix<T>& m);
+}
 
 #endif
--- src/Standalone.hpp
+++ src/Standalone.hpp
@@ -1,24 +1,49 @@
 #include "Standalone.h"
 
-float gaussianRandom() {
-	float v1, v2, s;
-	do {
-		v1 = 2.0f * (float)(rand() % 10000) / (10000) - 1.0f;
-		v2 = 2.0f * (float)(rand() % 10000) / (10000) - 1.0f;
-		s = v1 * v1 + v2 * v2;
-	} while (s >= 1.0f || s == 0.0f);
+namespace random {
+	float gaussianRandom() {
+		float v1, v2, s;
+		do {
+			v1 = 2.0f * (float)(rand() % 10000) / (10000) - 1.0f;
+			v2 = 2.0f * (float)(rand() % 10000) / (10000) - 1.0f;
+			s = v1 * v1 + v2 * v2;
+		} while (s >= 1.0f || s == 0.0f);
 
-	s = (float)pow((-2.0f * log(s)) / s, 0.5f);
+		s = (float)pow((-2.0f * log(s)) / s, 0.5f);
 
-	return v1 * s;
+		return v1 * s;
+	}
+
+	float uniformFloatRandom()
+	{
+		return static_cast<float>(rand() % 10000) / (10000);
+	}
+
+	int uniformIntRandom(int max)
+	{
+		return rand() % max;
+	}
 }
 
-float uniformFloatRandom()
+namespace tenseopr
 {
-	return (float)(rand() % 10000) / (10000);
-}
+	templ Matrix<T> abs(const Matrix<T>& m)
+	{
+		Matrix<T> mat(m);
+		void(*fPtr)(int&) = [](int& val) {val = ::abs(val);};
 
-int uniformIntRandom()
-{
-	return rand() % INT_MAX;
+		mat.fill(fPtr);
+		return mat;
+	}
+	
+	templ T accu(const Matrix<T>& m)
+	{
+		T sum = 0;
+		for (typename Matrix<T>::const_iterator itr = m.begin();itr != m.end();++itr) {
+			sum += *itr;
+		}
+		return sum;
+	}
+
+
 }
