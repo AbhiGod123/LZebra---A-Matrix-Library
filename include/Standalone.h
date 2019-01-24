9d407d4d38b1e3e623cf6910d9b8dc636df3c1da
Abhinav Neelam <abhinavneelam949@gmail.com>
2019-01-23 17:54:40 -0800

    Added common functions of vec and matrix

--- include/Standalone.h
+++ include/Standalone.h
@@ -3,9 +3,16 @@
 
 #define _USE_MATH_DEFINES
 #define templ template<typename T>
+#define cmat const Matrix<T>& 
+#define ccvec const ColVector<T>& 
+#define crvec const RowVector<T>& 
 
 template<typename T>
 class Matrix;
+template<typename T>
+class ColVector;
+template<typename T>
+class RowVector;
 
 #include <cmath>
 #include <limits>
@@ -17,8 +24,23 @@ namespace random {
 }
 
 namespace tenseopr {
-	templ Matrix<T> abs(const Matrix<T>& m);
-	templ T accu(const Matrix<T>& m);
+	templ Matrix<T> abs(cmat m);
+	templ T accu(cmat m);
+	templ T as_scalar(cmat m);
+	templ Matrix<T> clamp(cmat m, T min, T max);
+	templ ColVector<T> cross(ccvec v1, ccvec v2);
+	templ RowVector<T> cross(crvec v1, crvec v2);
+	templ Matrix<T> cumsum(cmat m, size_t dim = 0); //DOESNT WORK
+	templ Matrix<T> cumprod(cmat m, size_t dim = 0); //DOESNT WORK
+	templ double dot(ccvec v1, ccvec v2);
+	templ double dot(crvec v1, crvec v2);
+	templ double norm_dot(ccvec v1, ccvec v2);
+	templ double norm_dot(crvec v1, crvec v2);
+	templ double magnitude(ccvec v1);
+	templ double magnitude(crvec v1);
+	templ ColVector<T> find(cmat m);
+	templ ColVector<T> find(std::function<void(T&)>);
+	templ ColVector<T> find(void(*func_p)(T&));
 }
 
 #endif
--- src/Standalone.hpp
+++ src/Standalone.hpp
@@ -27,7 +27,7 @@ namespace random {
 
 namespace tenseopr
 {
-	templ Matrix<T> abs(const Matrix<T>& m)
+	templ Matrix<T> abs(cmat m)
 	{
 		Matrix<T> mat(m);
 		void(*fPtr)(int&) = [](int& val) {val = ::abs(val);};
@@ -36,7 +36,7 @@ namespace tenseopr
 		return mat;
 	}
 	
-	templ T accu(const Matrix<T>& m)
+	templ T accu(cmat m)
 	{
 		T sum = 0;
 		for (typename Matrix<T>::const_iterator itr = m.begin();itr != m.end();++itr) {
@@ -45,5 +45,168 @@ namespace tenseopr
 		return sum;
 	}
 
+	templ T as_scalar(cmat m) {
+		return m[0];
+	}
 
+	templ Matrix<T> clamp(cmat m,T min, T max) {
+		Matrix<T> mat(m);
+
+		for (typename Matrix<T>::iterator itr = mat.begin();itr != mat.end();++itr) {
+			if (*itr < min)
+				*itr = min;
+			else if (*itr > max)
+				*itr = max;
+		}
+		return mat;
+	}
+
+	templ ColVector<T> cross(ccvec v1, ccvec v2) {
+		ColVector<T> vec(3);
+
+		size_t x = 0;
+		size_t y = 1;
+		size_t z = 2;
+
+		vec(x) = v1(y)*v2(z) - v1(z)*v2(y);
+		vec(y) = v1(z)*v2(x) - v1(x)*v2(z);
+		vec(z) = v1(x)*v2(y) - v1(y)*v2(x);
+
+		return vec;
+	}
+
+	templ RowVector<T> cross(crvec v1, crvec v2) {
+		RowVector<T> vec(3);
+
+		size_t x = 0;
+		size_t y = 1;
+		size_t z = 2;
+
+		vec(x) = v1(y)*v2(z) - v1(z)*v2(y);
+		vec(y) = v1(z)*v2(x) - v1(x)*v2(z);
+		vec(z) = v1(x)*v2(y) - v1(y)*v2(x);
+
+		return vec;
+	}
+
+	templ Matrix<T> cumsum(cmat m, size_t dim)
+	{
+		Matrix<T> mat(m);
+
+		switch (dim) {
+			case 0: {
+				for (size_t i = 0;i < m.getRows();++i) {
+					typename Matrix<T>::col_iterator itrend = mat.end_row(i);
+
+					T cm = 0;
+					for (typename Matrix<T>::col_iterator itr1 = mat.begin_row(i);itr1 != itrend;++itr1) {
+						cm += *itr1;
+						*itr1 = cm;
+					}
+				}
+				break;
+			}
+			case 1: {
+				for (size_t i = 0;i < m.getCols();++i) {
+					typename Matrix<T>::row_iterator itrend = mat.end_col(i);
+					
+					T cm = 0;
+					for (typename Matrix<T>::row_iterator itr1 = mat.begin_col(i); itr1 != itrend;++itr1) {
+						cm += *itr1;
+						std::cout << cm << std::endl;
+						*itr1 = cm;
+						
+					}
+				}
+				break;
+			}
+		}
+
+		return mat;
+	}
+
+	templ Matrix<T> cumprod(cmat m, size_t dim)
+	{
+		Matrix<T> mat;
+		mat.copysize(m);
+
+		switch (dim) {
+			case 0: {
+				for (size_t i = 0;i < m.getRows();++i) {
+					typename Matrix<T>::col_iterator itrend = mat.end_row(i);
+
+					T cm = 0;
+					for (typename Matrix<T>::col_iterator itr1 = mat.begin_row(i);itr1 != itrend;++itr1) {
+						cm *= *itr1;
+						*itr1 = cm;
+					}
+				}
+				break;
+			}
+			case 1: {
+				for (size_t i = 0;i < m.getCols();++i) {
+					typename Matrix<T>::row_iterator itrend = mat.end_col(i);
+
+					T cm = 0;
+					for (typename Matrix<T>::row_iterator itr1 = mat.begin_col(i); itr1 != itrend;++itr1) {
+						cm *= *itr1;
+						std::cout << cm << std::endl;
+						*itr1 = cm;
+
+					}
+				}
+				break;
+			}
+		}
+
+		return mat;
+	}
+
+	templ double dot(ccvec v1, ccvec v2)
+	{
+		double sum = 0;
+		for (size_t i = 0;i < v1.getSize();++i) {
+			sum+= v1(i) * v2(i);
+		}
+		return sum;
+	}
+
+	templ double dot(crvec v1, crvec v2) 
+	{
+		double sum = 0;
+		for (size_t i = 0;i < v1.getSize();++i) {
+			sum += v1(i) * v2(i);
+		}
+		return sum;
+	}
+
+	templ double norm_dot(ccvec v1, ccvec v2)
+	{
+		return dot(v1, v2) / (magnitude(v1) * magnitude(v2));
+	}
+
+	templ double norm_dot(crvec v1, crvec v2)
+	{
+		return dot(v1, v2) / (magnitude(v1) * magnitude(v2));
+	}
+
+	templ double magnitude(ccvec v1)
+	{
+		double sum = 0;
+
+		for (size_t i = 0;i < v1.getSize();++i) {
+			sum += ::pow(v1(i),2);
+		}
+		return pow(sum,0.5);
+	}
+
+	templ double magnitude(crvec v1)
+	{
+		double sum = 0;
+
+		for (size_t i = 0;i < v1.getSize();++i) {
+			sum += ::pow(v1(i), 2);
+		}
+		return pow(sum, 0.5);
+	}
 }
