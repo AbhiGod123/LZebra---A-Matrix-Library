b6c0766fc95a2f7dccf3c1f5a5960836157151c9
Abhinav Neelam <abhinavneelam949@gmail.com>
2019-02-04 23:41:34 -0800

    Added decomp files

---
+++ include/Decomposition.h
@@ -0,0 +1,9 @@
+#ifndef DECOMP_H
+#define DECOMP_H
+
+#include "Include.h"
+
+namespace decomp {
+	templ void eig_sym();
+}
+#endif // !DECOMP_H
---
+++ src/Decomposition.hpp
@@ -0,0 +1,10 @@
+#ifndef DECOMP_HPP
+#define DECOMP_HPP
+
+#include "Decomposition.h"
+
+namespace decomp {
+
+}
+
+#endif // !DECOMP_HPP
