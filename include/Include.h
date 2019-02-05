3f234eb22a8797080399b4dae822bbd58ba33ff3
Abhinav Neelam <abhinavneelam949@gmail.com>
2019-02-04 23:40:57 -0800

    pushed includes into one file

---
+++ include/Include.h
@@ -0,0 +1,37 @@
+#ifndef INCLUDE_H
+#define INCLUDE_H
+
+#define _USE_MATH_DEFINES
+
+#include <vector>
+#include <cmath>
+#include <complex>
+#include <random>
+#include <algorithm>
+#include <numeric>
+#include <tuple>
+#include <limits>
+#include <type_traits>
+
+#define templ template<typename T>
+#define cmat const Matrix<T>&
+#define noncmat Matrix<T>&
+#define ccvec const ColVector<T>& 
+#define crvec const RowVector<T>& 
+#define cpmat const Matrix<std::complex<T>>&
+#define noncpmat Matrix<std::complex<T>>&
+#define _fPtr template<double(*_func)(double), typename T>
+
+#define ftempldec template<typename T, typename = typename std::enable_if<std::is_floating_point<T>::value>::type>
+#define ftempldef template<typename T, typename>
+
+typedef unsigned char uchar;
+
+template<typename T>
+class Matrix;
+template<typename T>
+class ColVector;
+template<typename T>
+class RowVector;
+
+#endif // !INCLUDE_H
